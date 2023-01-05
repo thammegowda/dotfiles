c0a810e18a4bcff9e6691bd6ab8c3d9d293a577d
Thamme Gowda <thammegowda@users.noreply.github.com>
2023-01-04 19:34:05 -0800

    automate conda setup

--- setup.bash
+++ setup.bash
@@ -19,4 +19,13 @@ for f in .tmux.conf .emacs.d; do
     ln -s $DOTFS/$f $HOME/$f
 done
 
+
+which conda || {
+  log "Installing miniconda3..."
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda-setup.sh \
+  && bash conda-setup.sh -b -p ~/miniconda3 \
+  && ~/miniconda3/bin/conda init bash \
+  && rm conda-setup.sh
+}
+
 log "Installation done. Open new shell to initialize new environment"
