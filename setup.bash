538b9b45e91afa867a86424ef883693b0af37ac7
Thamme Gowda <thammegowda@users.noreply.github.com>
2025-08-17 12:52:06 -0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -42,7 +42,7 @@ set -eux
 # install git credential manager if necessary; assuming linux_amd64
 which git-credential-manager || {
     GCM_URL="https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.1/gcm-linux_amd64.2.6.1.deb"   
-    GCM_FILE=$(basename $URL)
+    GCM_FILE=$(basename $GCM_URL)
     wget $GCM_URL
     sudo dpkg -i $GCM_FILE
     rm -f $GCM_FILE
